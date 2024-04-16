38456eb0a1b16339e34e63eb33d40896b5a19761
Dylan Backus <duradylan@gmail.com>
2024-04-16 13:09:32 +0200

    test

---
+++ resources/views/index.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Groeicafe</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  </style>
+</head>
+<body>
+    <div class="promo-top">
+        <div class="promo1">
+            <h1 class="promo-text">Voor 23:45 besteld morgen in huis</h1>
+        </div>
+        <div class="promo2">
+            <h1 class="promo-text">Achteraf betalen met klarna</h1>
+        </div>
+        <div class="promo3">
+            <h1 class="promo-text">100% Hoogwaardige ingredienten</h1>
+        </div>
+        <div class="promo4">
+            <h1 class="promo-text">Bestellingen van €30+ gratis verzending</h1>
+        </div>
+        <div class="button-container" href="/login">
+            <a class="login-button" href="/login">Login</a>
+        </div>
+    </div>
+        <img src="media/samsulek.png" class="main-image">
+        <div class="image-overlay">
+            <div class="groeicafe-text-container">
+                <h1 class="groeicafe-text">HET GROEICAFE</h1>
+            </div>
+            <div class="containers-container">
+                <div class="container-links">
+                    <h1 class="nieuw-bij-text">NIEUW BIJ GROEICAFE</h1>
+                    <h1 class="studentenpakketten-text">STUDENTENPAKKETTEN</h1>
+                    <h1 class="zorgvuldig-text">Zorgvuldig samengestelde pakketten passend voor iedere doelstelling.</h1>
+                    <a class="shop-nu-button">SHOP NU ALLE PAKKETEN</a>
+                </div>
+                </div>
+            </div>
+        </div>
+    <div class="footer-gold-bar">
+
+    </div>
+    <div class="footer">
+        <h1 class="footer-main-text">HET GROEICAFE</h1>
+        <div class="footer-main-container">
+            <div class="footer-1-containers">
+                <h1 class="footer-containers-main-text">VOLG ONS<h1>
+            </div>
+            <div class="footer-2-containers">
+                <h1 class="footer-containers-main-text">LINKS<h1>
+                <h1 class="footer-containers-second-text">Home</h1>
+                <h1 class="footer-containers-second-text">Shop</h1>
+                <h1 class="footer-containers-second-text">Zoekopdracht</h1>
+            </div>
+            <div class="footer-3-containers">
+                <h1 class="footer-containers-main-text">KLANTENSERVICE<h1>
+                <h1 class="footer-containers-second-text">Contact</h1>
+                <h1 class="footer-containers-second-text">Verzendbeleid</h1>
+                <h1 class="footer-containers-second-text">Terugbetalingsbeleid</h1>
+                <h1 class="footer-containers-second-text">Servicevoorwaarden</h1>
+                <h1 class="footer-containers-second-text">Privacybeleid</h1>
+
+            </div>
+            <div class="footer-4-containers">
+               <h1 class="footer-containers-main-text">NIEUWSBRIEF<h1>
+               <h1 class="footer-containers-second-text">Promoties, nieuwe producten en uitverkoop. Rechtstreeks in uw inbox.<h1>
+            </div>
+        </div>
+        <div class="footer-onder-container">
+
+        </div>
+    </div>
+</body>
+</html>
